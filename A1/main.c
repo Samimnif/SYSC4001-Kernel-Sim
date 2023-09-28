9139aa2f35e59ba44c0fd83dd6aa516cfd30fc25
Sami Mnif <samimnif@icloud.com>
2023-09-28 13:02:05 -0400

    Initialized main.c & added structure

---
+++ A1/main.c
@@ -0,0 +1,19 @@
+/**
+ * SYSC 4001 - Assignment 1
+ * 
+ * Date: 28-09-2023
+ * @author Sami Mnif - 101199669
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
