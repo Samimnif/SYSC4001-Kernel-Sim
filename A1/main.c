b00c3af348bb38b6c4099317769a6aeec1fdc5ac
Sami Mnif <samimnif@icloud.com>
2023-09-29 22:18:14 -0400

    Added Structures & Function for init process

--- A1/main.c
+++ A1/main.c
@@ -1,10 +1,10 @@
 /**
  * SYSC 4001 - Assignment 1
- * 
+ *
  * Date: 28-09-2023
  * @author Sami Mnif - 101199669
- * 
-*/
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,22 +12,50 @@
 #include <stdbool.h>
 #include <math.h>
 
+enum STATUS_ENUM
+{
+    NEW,
+    READY,
+    RUNNING,
+    WAITING,
+    TERMINATED
+};
 
-struct process {
+static const char *STATUS_STRING[] = {
+    "NEW",
+    "READY",
+    "RUNNING",
+    "WAITING",
+    "TERMINATED"
+};
+
+struct process
+{
     int PID;
     int arrival_time;
     int CPU_time;
     int IO_frequency;
     int IO_duration;
-
 };
 
-struct task{
-    struct process* p;
-    struct task* next;
+struct task
+{
+    struct process *p;
+    struct task *next;
 };
 
-
+/**
+ * ß
+*/
+struct process* create_process(int PID, int arrival_time, int CPU_time, int IO_frequency, int IO_duration){
+    struct process* new_process = (struct process*) malloc(sizeof(struct process));
+    new_process->PID = PID;
+    new_process->arrival_time = arrival_time;
+    new_process->CPU_time = CPU_time;
+    new_process->IO_frequency = IO_frequency;
+    new_process->IO_duration = IO_duration;
+    return &new_process;
+}
 
 int main(int argc, char const *argv[])
 {
