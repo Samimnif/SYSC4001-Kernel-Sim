4cb354c8f10a330b46c83aa39c5393246ce38cf2
Sami Mnif <samimnif@icloud.com>
2023-10-01 22:59:08 -0400

    added node_t functions to handle linked list

--- A1/main.c
+++ A1/main.c
@@ -1,7 +1,6 @@
 /**
  * SYSC 4001 - Assignment 1
  *
- * Date: 28-09-2023
  * @author Sami Mnif - 101199669
  *
  */
@@ -26,29 +25,29 @@ static const char *STATUS_STRING[] = {
     "READY",
     "RUNNING",
     "WAITING",
-    "TERMINATED"
-};
+    "TERMINATED"};
 
-struct process
+typedef struct Process
 {
     int PID;
     int arrival_time;
     int CPU_time;
     int IO_frequency;
     int IO_duration;
-};
+} process;
 
-struct task
+typedef struct node
 {
-    struct process *p;
-    struct task *next;
-};
+    process *p;
+    struct node *next;
+} node_t;
 
 /**
- * ß
-*/
-struct process* create_process(int PID, int arrival_time, int CPU_time, int IO_frequency, int IO_duration){
-    struct process* new_process = (struct process*) malloc(sizeof(struct process));
+ *
+ */
+process *create_process(int PID, int arrival_time, int CPU_time, int IO_frequency, int IO_duration)
+{
+    process *new_process = (process *)malloc(sizeof(process));
     new_process->PID = PID;
     new_process->arrival_time = arrival_time;
     new_process->CPU_time = CPU_time;
@@ -57,8 +56,73 @@ struct process* create_process(int PID, int arrival_time, int CPU_time, int IO_f
     return &new_process;
 }
 
+node_t *create_node(process *p)
+{
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->p = p;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+node_t *add_node(node_t *n, node_t *head)
+{
+    if (head == NULL)
+    {
+        head = n;
+    }
+    else
+    {
+        node_t *current = head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = n;
+    }
+    return head;
+}
+
+bool remove_node(node_t *n, node_t *head)
+{
+    if (head == n)
+    {
+        head = head->next;
+        n->next = NULL; // to isolate the node from the list
+        return true;
+    }
+    else
+    {
+        node_t *current = head;
+        node_t *prev = NULL;
+        while (current->next != NULL)
+        {
+            if (current == n)
+            {
+                prev->next = current->next;
+                n->next = NULL;
+                return true;
+            }
+            prev = current;
+            current = current->next;
+        }
+    }
+    return false;
+}
+
+
+node_t fetch_data(FILE fp){
+    
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    bool exit = false;
+    int clock = 0;
+
+    do
+    {
+
+    } while (!exit);
     return 0;
 }
