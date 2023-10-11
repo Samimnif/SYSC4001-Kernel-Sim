03b5fa062ecd548369301dece0b80bddd293336a
Sami Mnif <samimnif@icloud.com>
2023-10-11 00:34:30 -0400

    Completed sim algorithm

--- A1/main.c
+++ A1/main.c
@@ -33,9 +33,9 @@ typedef struct Process
     int arrival_time;
     int CPU_time;
     int IO_frequency;
-    int IO_Clock;
+    int IO_Fclock;
     int IO_duration;
-    int IO_left;
+    int IO_Dleft;
     enum P_STATUS status;
 } process;
 
@@ -55,9 +55,9 @@ process *create_process(int PID, int arrival_time, int CPU_time, int IO_frequenc
     new_process->arrival_time = arrival_time;
     new_process->CPU_time = CPU_time;
     new_process->IO_frequency = IO_frequency;
-    new_process->IO_Clock = IO_frequency;
+    new_process->IO_Fclock = IO_frequency;
     new_process->IO_duration = IO_duration;
-    new_process->IO_left = IO_duration;
+    new_process->IO_Dleft = IO_duration;
     new_process->status = NEW;
     return new_process;
 }
@@ -100,7 +100,7 @@ node_t *remove_node(node_t *n, node_t *head)
     {
         node_t *current = head;
         node_t *prev = NULL;
-        while (current->next != NULL)
+        while (current != NULL)
         {
             if (current == n)
             {
@@ -113,6 +113,19 @@ node_t *remove_node(node_t *n, node_t *head)
         }
     }
     return head;
+}
+
+node_t *getFirst_node(node_t **head)
+{
+    node_t *temp;
+    if (head != NULL)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        temp->next = NULL;
+        return temp;
+    }
+    return NULL;
 }
 
 void print_list(node_t *head)
@@ -172,54 +185,155 @@ node_t *fetch_data(char *filename)
     return data;
 }
 
+void write_process(char outputFile[], int counter, int PID, const char old_state[], const char new_state[]){
+	FILE *output;
+	output = fopen(outputFile , "a");
+	char counter_char[1000];
+	sprintf(counter_char, "%d", counter);
+	char PID_char[1000];
+	sprintf(PID_char, "%d", PID);
+	fputs(counter_char, output);
+	fputs(",", output);
+	fputs(PID_char, output);
+	fputs(",", output);
+	fputs(old_state, output);
+	fputs(",", output);
+	fputs(new_state, output);
+	fputs("\n", output);
+	fclose(output);
+}
+
 int main(int argc, char const *argv[])
 {
     // char *filename = argv[1];
     char filename[] = "test_case_1.csv";
+    char outputFile[] = "output-";
+    printf("\n Output: %s  **\n",outputFile);
+	printf("Input %s  **\n",filename);
     node_t *data = fetch_data(filename);
-    node_t *newList = NULL, *readyList = NULL, *waitingList = NULL, *terminatedList = NULL, *running = NULL, *listHead;
+    strcat(outputFile, filename);
+    FILE *output;
+	output = fopen(outputFile , "a");
+	fputs("Time of transition", output);
+	fputs(",", output);
+	fputs("PID", output);
+	fputs(",", output);
+	fputs("Old State", output);
+	fputs(",", output);
+	fputs("New State", output);
+	fputs("\n", output);
+	fclose(output);
+
+    node_t *newList = NULL, *readyList = NULL, *waitingList = NULL, *terminatedList = NULL, *running = NULL, *listHead, *tempNext;
+    print_listln(data);
 
     bool exit = false;
     int clock = 0;
 
     do
     {
-        printf("CLOCK %d", clock);
+        printf("CLOCK %d\n", clock);
         printf("RUNNING: ");
         print_listln(running);
         printf("NEW List: ");
-        print_listln(newList);
+        print_listln(data);
         printf("READY List: ");
         print_listln(readyList);
         printf("IO List: ");
         print_listln(waitingList);
         printf("Terminaed List: ");
         print_listln(terminatedList);
+        printf("\n");
+        // IO List Check to prepare the READY List
         listHead = waitingList;
         while (listHead != NULL)
         {
-            if (listHead->p->IO_left == 0)
+            listHead->p->IO_Dleft -= 1;
+            if (listHead->p->IO_Dleft == 0)
             {
+                tempNext = listHead->next;
+                // Change of STATE here from WAITING to READY
+                write_process(outputFile, clock, listHead->p->PID, STATUS_STRING[listHead->p->status], STATUS_STRING[READY]);
+                listHead->p->status = READY;
+                waitingList = remove_node(listHead, waitingList);
+                readyList = add_node(listHead, readyList);
+                listHead = tempNext;
             }
-            listHead = listHead->next;
+            else
+            {
+                listHead = listHead->next;
+            }
         }
-        listHead = newList;
+        // NEW arriving Tasks to be adde to READY Queue
+        listHead = data;
         while (listHead != NULL)
         {
-            listHead = listHead->next;
+            if (listHead->p->arrival_time == clock)
+            {
+                tempNext = listHead->next;
+                // Chnage of STATE here from WAITING to READY
+                write_process(outputFile, clock, listHead->p->PID, STATUS_STRING[listHead->p->status], STATUS_STRING[READY]);
+                listHead->p->status = READY;
+                data = remove_node(listHead, data);
+                readyList = add_node(listHead, readyList);
+                listHead = tempNext;
+            }
+            else
+            {
+                listHead = listHead->next;
+            }
         }
-        if (running == NULL)
+        if (running != NULL)
         {
+            running->p->IO_Fclock -= 1;
+            running->p->CPU_time -= 1;
+            if (running->p->CPU_time == 0)
+            {
+                // Process ended and changed to TERMINATED
+                write_process(outputFile, clock, running->p->PID, STATUS_STRING[running->p->status], STATUS_STRING[TERMINATED]);
+                running->p->status = TERMINATED;
+                terminatedList = add_node(running, terminatedList);
+                running = NULL;
+            }
+            else if (running->p->IO_Fclock == 0)
+            {
+                // Process needs IO access, status changed to WAITING
+                write_process(outputFile, clock, running->p->PID, STATUS_STRING[running->p->status], STATUS_STRING[WAITING]);
+                running->p->status = WAITING;
+                running->p->IO_Dleft = running->p->IO_duration;
+                waitingList = add_node(running, waitingList);
+                running = NULL;
+            }
+        }
+        else
+        {
+            if (readyList != NULL)
+            {
+                running = add_node(getFirst_node(&readyList), running);
+                // Process moved to CPU. State changed from READY to RUNNING
+                write_process(outputFile, clock, running->p->PID, STATUS_STRING[running->p->status], STATUS_STRING[RUNNING]);
+                running->p->status = RUNNING;
+                running->p->IO_Fclock = running->p->IO_frequency;
+            }
+            else
+            {
+                printf("READY QUEUE EMPTY\nCPU STALL\n");
+            }
+        }
+        if (readyList == NULL && running == NULL && data == NULL && waitingList == NULL)
+        {
+            exit = true;
         }
         clock++;
+        //sleep(1);
 
     } while (!exit);
 
     // Freeing the Heap
-    while (data != NULL)
+    while (terminatedList != NULL)
     {
-        node_t *temp = data;
-        data = data->next;
+        node_t *temp = terminatedList;
+        terminatedList = terminatedList->next;
         free(temp->p);
         free(temp);
     }
