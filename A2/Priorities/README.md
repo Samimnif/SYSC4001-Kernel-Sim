d8fc4419c3086831c68c1e95378f6f6a88766e25
Sami Mnif <samimnif@icloud.com>
2023-11-10 23:35:35 -0500

    Fixed Priority main.c -> getHighestPriority function fixed

---
+++ A2/Priorities/README.md
@@ -0,0 +1,22 @@
+# Assignement 2
+## External Priorities (no preemption) Algorithm
+In main.c we implemented the External Priorities (no preemption) Algorithm.
+### Results
+* Test Case 1
+```text
+STATS:
+PID:1001      Waiting Time:0     Turnaround:100
+PID:1002      Waiting Time:0     Turnaround:200
+PID:1003      Waiting Time:10     Turnaround:120
+PID:1004      Waiting Time:0     Turnaround:110
+Average Waiting Time: 2.500000
+Average Turnaround Time: 132.500000
+```
+* Test Case 2
+```text
+
+```
+* Test Case 3
+```text
+
+```
--- A2/Priorities/main.c
+++ A2/Priorities/main.c
@@ -21,6 +21,7 @@ typedef struct Process
     int IO_duration;
     int IO_duration_left;
     int waiting_time;
+    int end_time; // for stats purposes only
     int priority;
 } process;
 
@@ -48,6 +49,7 @@ process *create_process(int PID, int arrival_time, int CPU_time, int IO_frequenc
     new_process->IO_duration = IO_duration;
     new_process->IO_duration_left = IO_duration;
     new_process->waiting_time = 0;
+    new_process->end_time = 0;
     new_process->priority = priority;
     return new_process;
 }
@@ -153,19 +155,19 @@ node_t *getHighestPriorityNode(node_t **head) {
     while (current != NULL) {
         if (current->p->priority > highestPriorityNode->p->priority) {
             highestPriorityNode = current;
-            previous = previous; // Store the previous node
+            //previous = previous; // Store the previous node
         }
-        previous = current;
+        //previous = current;
         current = current->next;
     }
-
+    /*
     if (previous == NULL) {
         *head = (*head)->next;
     } else {
         previous->next = highestPriorityNode->next;
     }
-
     highestPriorityNode->next = NULL;
+    */
     return highestPriorityNode;
 }
 
@@ -293,7 +295,7 @@ int main(int argc, char const *argv[])
     fclose(output);
 
     // Initialize the Queue/list for each state: READY, RUNNING, WAITING,TERMINATED
-    node_t *readyList = NULL, *waitingList = NULL, *terminatedList = NULL, *running = NULL, *listHead, *tempNext; // Added some helper variables too
+    node_t *readyList = NULL, *waitingList = NULL, *terminatedList = NULL, *running = NULL, *listHead, *tempNext, *highestPriority; // Added some helper variables too
     //print_listln(data);
 
     bool exit = false;
@@ -375,6 +377,7 @@ int main(int argc, char const *argv[])
             {
                 // Process ended and changed to TERMINATED
                 write_process(outputFile, clock, running->p->PID, "RUNNING", "TERMINATED");
+                running->p->end_time = clock;
                 terminatedList = add_node(running, terminatedList);
                 running = NULL;
             }
@@ -391,7 +394,9 @@ int main(int argc, char const *argv[])
             {
                 if (readyList != NULL)
                 {
-                    running = add_node(getFirst_node(&readyList), running);
+                    highestPriority = getHighestPriorityNode(&readyList);
+                    readyList = remove_node(highestPriority, readyList);
+                    running = add_node(highestPriority, running);
                     // Process moved to CPU. State changed from READY to RUNNING
                     write_process(outputFile, clock, running->p->PID, "READY", "RUNNING");
                     running->p->IO_frequency_left = running->p->IO_frequency;
@@ -406,7 +411,10 @@ int main(int argc, char const *argv[])
         {
             if (readyList != NULL)
             {
-                running = add_node(getHighestPriorityNode(&readyList), running);
+                highestPriority = getHighestPriorityNode(&readyList);
+                readyList = remove_node(highestPriority, readyList);
+                running = add_node(highestPriority, running);
+                //running = add_node(getHighestPriorityNode(&readyList), running);
                 // Process moved to CPU. State changed from READY to RUNNING
                 write_process(outputFile, clock, running->p->PID, "READY", "RUNNING");
                 running->p->IO_frequency_left = running->p->IO_frequency;
@@ -426,19 +434,21 @@ int main(int argc, char const *argv[])
     }
 
     printf("\n===================\nTERMINATED\n\nSTATS:\n");
-    float average_t = 0;
+    float average_waiting = 0, average_turnaround = 0;
     int process_num = 0;
     listHead = terminatedList;
     //Going through all Process in the TERMINATED list and collect info like waiting time and number of process
     while (listHead != NULL)
     {
-        printf("PID:%d      Waiting Time:%d\n", listHead->p->PID, listHead->p->waiting_time);
+        printf("PID:%d      Waiting Time:%d     Turnaround:%d\n", listHead->p->PID, listHead->p->waiting_time, listHead->p->end_time - listHead->p->arrival_time);
         process_num++;
-        average_t += listHead->p->waiting_time;
+        average_waiting += listHead->p->waiting_time;
+        average_turnaround += listHead->p->end_time - listHead->p->arrival_time;
         listHead = listHead->next;
     }
     //Calculating Avergae Waiting Time and Printing it to thr Terminal
-    printf("Average Waiting Time: %f\n", (average_t / process_num));
+    printf("Average Waiting Time: %f\n", (average_waiting / process_num));
+    printf("Average Turnaround Time: %f\n", (average_turnaround / process_num));
 
     // Freeing the Heap
     while (terminatedList != NULL)
